b5ff6253c13bd3b5a2df97134f3ab85212a33648
zulfi1098 <zulfi1098@yahoo.com>
2018-06-03 03:36:26 +0400

    Revert "My submission"
    This reverts commit cd726dc4e891c8c098794cce3f62e0a1d2e5c088.

--- CrossSolar/Controllers/PanelController.cs
+++ CrossSolar/Controllers/PanelController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CrossSolar.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class PanelController : Controller
     {
         private readonly IPanelRepository _panelRepository;
@@ -17,9 +17,8 @@ namespace CrossSolar.Controllers
         {
             _panelRepository = panelRepository;
         }
-
+       
         // POST api/panel
-
         [HttpPost]
         public async Task<IActionResult> Register([FromBody]PanelModel value)
         {
